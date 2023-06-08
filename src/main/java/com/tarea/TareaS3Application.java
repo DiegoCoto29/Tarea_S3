ffa9abd69768afc94aaa315c71e9b3cfda37d1ce
Diego <Diego@Coto>
2023-06-07 19:21:52 -0600

    Tarea_S3

---
+++ src/main/java/com/tarea/TareaS3Application.java
@@ -0,0 +1,13 @@
+package com.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaS3Application.class, args);
+	}
+
+}
